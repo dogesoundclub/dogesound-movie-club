e1ed4af822f084c5326bc76c5d74d2fb62537b9d
Hanul <hanul@hanul.me>
2021-10-19 23:10:03 +0900

    개발 완료

---
+++ contracts/DogeSoundMovieClip.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.6;
+
+import "./klaytn-contracts/token/KIP37/KIP37.sol";
+import "./klaytn-contracts/token/KIP37/KIP37Burnable.sol";
+import "./klaytn-contracts/token/KIP37/KIP37Pausable.sol";
+import "./klaytn-contracts/token/KIP37/KIP37Mintable.sol";
+
+contract DogeSoundMovieClip is KIP37, KIP37Burnable, KIP37Pausable, KIP37Mintable {
+    constructor() public KIP37("") {}
+}
